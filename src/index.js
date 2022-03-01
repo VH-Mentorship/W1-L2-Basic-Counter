69a92bfc154fdc3cf33b265f5120bb9ee562a77f
keshvim <keshmahal@gmail.com>
2022-02-28 22:04:13 -0600

    Added starter code files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+const counterDisplay = document.querySelector("#display");
+// assign the '+' button to a const named 'counterInc' here
+
+counterInc.addEventListener("click", () => {
+  // update count 
+  updateDisplay();
+});
+
+ // add an event listener for the '-' button here
+ //  don't forget to fetch the button element above!
+
+function updateDisplay() {
+  // update display
+}
